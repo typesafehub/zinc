7025d68be628035a9991b4f74dcdd62edacf0e92
Peter Vlugter <pvlugter@gmail.com>
2013-10-31 19:01:02 +1300

    Version 0.3.1-M1

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -12,7 +12,7 @@ object ZincBuild extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.typesafe.zinc",
-    version := "0.3.1-SNAPSHOT",
+    version := "0.3.1-M1",
     scalaVersion := "2.10.3",
     crossPaths := false
   )
