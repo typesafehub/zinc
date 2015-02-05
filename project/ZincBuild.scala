ac6446e695ab235f53274e4c510f2643f221d692
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2015-02-05 15:47:09 +0100

    Zinc 0.3.6 for sbt 0.13.6

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -12,7 +12,7 @@ object ZincBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.typesafe.zinc",
-    version := "0.3.6-SNAPSHOT",
+    version := "0.3.6",
     scalaVersion := "2.10.4",
     crossPaths := false
   )
