c088531458698d794640d05a07a2a598826efe4e
James Mouradian <jmouradian@twitter.com>
2015-04-16 15:26:38 -0700

    Made SimpleCompileProgress more style-compliant.

--- src/main/scala/com/typesafe/zinc/SimpleCompileProgress.scala
+++ src/main/scala/com/typesafe/zinc/SimpleCompileProgress.scala
@@ -1,20 +1,13 @@
 package com.typesafe.zinc
 
-import xsbti.compile.{ CompileProgress }
 import math.max
+import xsbti.compile.CompileProgress
 
-/**
- * Created by jmouradian on 4/14/15.
- */
 class SimpleCompileProgress extends CompileProgress {
-  var lastCurrent: Int = 0  // The last number of steps to have been completed.
+  var lastCurrent: Int = 0  
 
-  def startUnit(phase: String, unitPath: String) : Unit = {
-    // Notify the user of the current compilation phase.
-    println(phase + " " + unitPath + "...")
-  }
+  def startUnit(phase: String, unitPath: String): Unit =  println(phase + " " + unitPath + "...")
 
-  // Always advance. If there exists progress to report to the user, print to stdout.
   def advance(current: Int, total: Int): Boolean = {
     if (current > lastCurrent) println("Progress: [" + current + "/" + total + "]")
     lastCurrent = math.max(current, lastCurrent)
