3ac07515597af2aed1f5b1c9b2fae80cf8f66e6e
James Mouradian <jmouradian@twitter.com>
2015-04-16 19:25:56 -0700

    Adds booleans to guard outputs for addition of arguments in constructor.

--- src/main/scala/com/typesafe/zinc/SimpleCompileProgress.scala
+++ src/main/scala/com/typesafe/zinc/SimpleCompileProgress.scala
@@ -6,27 +6,32 @@ import xsbti.compile.CompileProgress
  * SimpleCompileProgress implements CompileProgress to add output to zinc scala compilations, but
  * does not implement the capability to cancel compilations via the `advance` method.
  */
-class SimpleCompileProgress extends CompileProgress {
-  /**
-   * lastCurrent tracks the latest reported number of currently completed compilation units.
-   */
+class SimpleCompileProgress (printUnits: Boolean, printProgress: Boolean) extends CompileProgress {
   var lastCurrent: Int = 0  
 
   /** 
-   * startUnit is called when SBT begins a compilation phase of a given file, and reports to stdout.
+   * startUnit Optionally reports to stdout when a phase of compilation has begun for a file.
    */
-  def startUnit(phase: String, unitPath: String): Unit =  println(phase + " " + unitPath + "...")
+  def startUnit(phase: String, unitPath: String): Unit =  {
+    if (printUnits) {
+      println(phase + " " + unitPath + "...")
+    }
+  }
 
   /**
+   * advance Optionally reports the number of compilation units completed out of the total.
+   * 
    * advance is periodically called during compilation, indicating the total number of compilation 
    * steps completed (`current`) out of the total number of steps necessary. The method returns 
    * false if the user wishes to cancel compilation, or true otherwise. Currently, Zinc never 
    * requests to cancel compilation.
    */
   def advance(current: Int, total: Int): Boolean = {
-    if (current > lastCurrent) {
-      println("Progress: [" + current + "/" + total + "]")
-      lastCurrent = current 
+    if (printProgress) {
+      if (current > lastCurrent) {
+        println("Progress: [" + current + "/" + total + "]")
+        lastCurrent = current 
+      }
     }
     true
   }
