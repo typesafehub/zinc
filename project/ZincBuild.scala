5d0224a42d5bbd7877d3901a9107b74bfa2206e1
Eugene Yokota <eed3si9n@gmail.com>
2015-09-10 01:30:13 -0400

    zinc 0.3.9. Fixes #85

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -12,7 +12,7 @@ object ZincBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.typesafe.zinc",
-    version := "0.3.9-SNAPSHOT",
+    version := "0.3.9",
     scalaVersion := "2.10.5",
     crossPaths := false
   )
